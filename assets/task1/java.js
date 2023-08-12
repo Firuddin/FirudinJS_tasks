5a72dbc187c738ecde614cfdcf13b58de9eed240
Firuddin <firuddinaliyev888@gmail.com>
2023-08-12 12:15:33 +0400

    add task-1

---
+++ assets/task1/java.js
@@ -0,0 +1,7 @@
+let doğulduğuil=prompt('doğum ilini daxil et')
+console.log(2023-doğulduğuil)
+
+
+let a  =(prompt('duzbucaqlinin uzunluqu'))
+let b  = (prompt ('duzbucaqlinin eni'))
+console.log(a*b)
