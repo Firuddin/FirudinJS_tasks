7c05760c18dc7f74b1bfbd422eb1590f721c3463
Firuddin <firuddinaliyev888@gmail.com>
2023-08-12 14:11:34 +0400

    add js

--- assets/task1/java.js
+++ assets/task1/java.js
@@ -5,3 +5,12 @@ console.log(2023-doğulduğuil)
 let a  =(prompt('duzbucaqlinin uzunluqu'))
 let b  = (prompt ('duzbucaqlinin eni'))
 console.log(a*b)
+
+
+let kilometr=Number(prompt('kilometr'))
+let mil=0.6214
+console.log(mil*kilometr)
+
+
+
+
