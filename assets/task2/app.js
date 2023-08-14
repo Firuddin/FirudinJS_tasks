c024cc6de1e2e7bd1b66c43f298e3c71f216e117
Firuddin <firuddinaliyev888@gmail.com>
2023-08-14 20:11:26 +0400

    add-task2

---
+++ assets/task2/app.js
@@ -0,0 +1,34 @@
+let ad= prompt("adinizi yazn")
+let soyad= prompt("soyadinizi yazn")
+let yaş=prompt("yaşinizi yazn")
+let netice=`
+Name: ${ad}
+Surname: ${soyad}
+Age: ${yaş}`
+console.log(netice)
+
+
+
+
+
+
+
+let r=prompt("radius")
+let n1=2
+let п=3
+let L=(r*n1*п)
+console.log(L)
+
+
+
+
+
+
+let n1=Number(prompt("ilk ededi daxil edin"))
+let n2=Number(prompt("ikinci ededi daxil edin"))
+let netice=`
+${n1}+${n2}=${n1+n2}
+${n1}*${n2}=${n1*n2}
+${n1}-${n2}=${n1-n2}
+${n1}/${n2}=${n1/n2}`
+console.log(netice)
