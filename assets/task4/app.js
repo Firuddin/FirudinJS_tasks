67cf6796ae41611506aab921ce21d73acd840521
Firuddin <firuddinaliyev888@gmail.com>
2023-08-19 11:30:38 +0400

    add commit

---
+++ assets/task4/app.js
@@ -0,0 +1,50 @@
+/*let weight = +prompt("Çəkini (kilqramda) daxil edin:");
+let height = +prompt("Boyunu (metrda) daxil edin:");
+
+let bmi = weight / (height * height);
+
+console.log("Sizin BMI-niz: " + bmi);
+
+if (bmi < 18.5) {
+  console.log("Fiona");
+} else if (bmi < 20) {
+  console.log("Normal");
+} else if (bmi < 25) {
+  console.log("şrek");
+} else if (bmi < 30) {
+  console.log("evini allah tiksin az ye");
+} else {
+  console.log("Obez");
+}
+*/
+
+
+let direction=prompt('isiqamet daxil edin(up,right,left,down')
+switch(direction){
+case'up':
+console.log('yuxari get')
+break
+case'right':
+console.log('sağa get')
+break
+case'left':
+console.log('sola get')
+break
+case'down':
+console.log('asaqi get')
+break
+default:
+    console.log('duzgun daxil etmirsen')
+}
+
+
+
+
+
+
+
+
+
+
+
+
