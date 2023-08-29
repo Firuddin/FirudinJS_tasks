6ff9b227586a5c7eec418a92676acf8c73258526
Firuddin <firuddinaliyev888@gmail.com>
2023-08-29 12:33:54 +0400

    add task

---
+++ assets/task5/app.js
@@ -0,0 +1,122 @@
+//task1
+
+/*const ed1=+prompt('ilk ededi daxil edin')
+const ed2=+prompt('ikinci ededi daxil edin')
+
+showMe(ed1,ed2,console.log)
+showMe(ed1,ed2,alert)
+
+function showMe(ed1,ed2,callback){
+callback(ed1+ed2)
+}*/
+
+
+
+
+
+
+
+
+//task2
+
+/*let gender =prompt('cinsinizi daxil edin')
+let name=prompt('adinizi daxil edin')
+ function show(gender, name){
+    if(gender==='k'||gender==='K')
+    {console.log(`salam cenab ${name}`)
+    }else if(gender==='q'||gender==='Q')
+    {console.log(`salam xanim ${name}`)
+    }else{
+        console.log('dur bas bayira')
+    }
+    
+ }
+show(gender,name)*/
+
+
+
+
+
+
+
+
+
+//task3
+
+/*let name=prompt('adinizi daxil edin')
+function logGreeting(name){
+    console.log(`${name} is a js developer`)
+}
+
+logGreeting(name)*/
+
+
+
+
+
+
+
+
+//task4
+
+/*function start(name){
+console.log(`${name} is active now`)
+}
+start('EliMuellim')
+start('Firudin')
+start('Eziz')*/
+
+
+
+
+
+
+
+
+//task5
+
+/*let number=prompt('ededi daxil edin')
+function fourth(number){
+    console.log(number**4)
+}
+fourth(number)*/
+
+
+
+
+
+
+
+
+//task6
+
+/*for(let i=100;i<1000;i+=10){
+    console.log(i)
+}*/
+
+
+
+
+
+
+
+
+
+//task7
+/* toplam=0
+for(let i=11;i<=99;i+=2){
+   toplam=toplam+i    
+}
+console.log(`butun iki reqemli tek ededlerin cemi ${toplam}`)*/
+
+
+
+
+
+
+
+
+
+
+
+
