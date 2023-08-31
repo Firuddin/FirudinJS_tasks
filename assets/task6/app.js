f206051a6631d0272ecb35b2a389e007cfad6865
Firuddin <firuddinaliyev888@gmail.com>
2023-08-31 16:27:10 +0400

    add

---
+++ assets/task6/app.js
@@ -0,0 +1,11 @@
+const prop2Taker = (obj)=>{
+
+        for(let key in obj){
+         if(key===`prop-2`){
+     
+             console.log(obj (key));
+         }
+        
+        }
+     }
+console.log('prop-2')
