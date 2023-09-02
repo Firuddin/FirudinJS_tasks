44252e1ad9ab7a81f8514c2b3228a18c07909184
Firuddin <firuddinaliyev888@gmail.com>
2023-09-02 11:32:05 +0400

    add

---
+++ assets/task6/app.js
@@ -0,0 +1,6 @@
+function prop2Taker(obj){
+    'prop-2':'ali'
+}
+
+prop2Taker(obj)
+console.log(prop-2)
---
+++ assets/task7/app.js
@@ -0,0 +1,18 @@
+const calc={
+    ed1:0,
+    ed2:0,
+    read() {
+        const ed1=+prompt('ilk ededi daxil edin')
+        const ed2=+prompt('ikinci ededi daxil edin')
+        this.ed1=ed1
+        this.ed2=ed2
+    },
+    Sum() {
+        console.log(this.ed1+this.ed2)
+    },
+    multiply(){},
+divide(){},
+subtract(){},
+}
+calc.read()
+calc.Sum()
