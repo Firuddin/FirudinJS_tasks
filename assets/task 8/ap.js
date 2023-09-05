3e8f86325411a5941c387186ff48f8320341993c
Firuddin <firuddinaliyev888@gmail.com>
2023-09-05 12:35:14 +0400

    add

---
+++ assets/task 8/ap.js
@@ -0,0 +1,52 @@
+//1.tapsiriq
+
+/*function repeatedSymbolCounter(ALIONALI, A) {
+  let count = 0;
+  for (let i=0; i < ALIONALI.length; i++) {
+    if (ALIONALI[i] === A) {
+      count++;
+    }
+  }
+  return count;
+}
+const netice=repeatedSymbolCounter('ALIONALI', 'A');
+console.log(netice)*/
+
+//2.tapsiriq
+
+/*function  capitalize (alion){
+    return alion[0].toUpperCase() + alion.slice(1)
+}
+console.log(capitalize('alion'))*/
+
+//3.tapsiriq
+
+/*function nameFormat(AlionGreenHeart){
+    return AlionGreenHeart.slice(0,7)
+}
+console.log(nameFormat('Alion GreenHeart'))*/
+
+//4.tapsiriq
+
+/*function ilkHerfiDeyis(alion, A){
+    return alion[0].toUpperCase() + alion.slice(1)
+}
+console.log(ilkHerfiDeyis('alion','A'))*/
+
+//5.tapsiriq
+
+/*function splitsence (sentence){
+    let write=sentence.split(' ')
+    return write
+}
+let sentence = 'Baqdagul qoncagul Mamed dersi qaynadirlar';
+let netice=splitsence(sentence)
+console.log(netice)*/
+
+//6.tapsiriq
+
+/*function removeCharacterFromString (Alion, A){
+return  Alion.slice(1,5);
+ }
+ let netice=removeCharacterFromString('Alion', 'A')
+ console.log(netice)*/
