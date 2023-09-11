8d7533b438cf37c044efad0770f98915b7b274c3
Firuddin <firuddinaliyev888@gmail.com>
2023-09-11 16:03:40 +0400

    new

---
+++ assets/task9/ap.js
@@ -0,0 +1,49 @@
+//task.1
+
+/*let arr = []
+arr.length=5
+let newarr=[]
+for(let i=0; i<arr.length;i++){
+    newarr[i]=+prompt('eded daxil edin')
+}
+arr=newarr
+console.log(newarr)*/
+
+
+
+
+
+
+
+
+//task.2
+
+/*let arr=['1','2','3','4','5','6']
+function myarr(arr){
+let newarr=1
+for(let i=0;i<arr.length;i++){
+    newarr=newarr*arr[i]
+    
+}
+return newarr
+}
+console.log(myarr(arr))*/
+
+
+
+
+
+
+
+
+//task3
+
+/*const multiply = (...arr)=>{
+hasil=1
+for(let value of arr){
+    hasil=hasil*value
+}
+return hasil
+}
+console.log(multiply(1,2,3));
+*/
