d2d0239e1cf4ca6a1a03e3b95f49230bd0b37c82
Firuddin <firuddinaliyev888@gmail.com>
2023-09-21 11:54:45 +0400

    add

---
+++ task.11 imt/ap.js
@@ -0,0 +1,44 @@
+/*task.3
+heap,stack
+let n1=25
+let n2=25
+burada deyisenin deyerleri bir birine beraber olsada,referans deyil ama...
+let n3=25
+let n4=n3
+console.log(n4)
+bele olan halda referanslari bir-birine beraberdir.stack bizim vizual sekilde kod yazdiqimiz yerdir.
+ama heap ise bizim gormediyimiz komputerin beyin terefi hesab olunur.
+
+
+
+task.2
+function
+ilk once qeyd edim ki funkiyalar 3-cur olunur
+1.arrow funksiyalari
+2.function expresion
+3.function defination
+function namber(p1, p2) {
+    return p1 * p2;
+  }
+  console.log(namber(5,8))
+  1.
+  name()=>{}                arrow function
+  2.
+  let add=name()={}          function expression
+
+
+  3.
+  function name(){}           function defination
+*/
+
+//task.3
+/*const obj={color:'red', car:'rangerower'} 
+  console.log(obj)*/
+//Obyektler massivlerin icerisende ve ya funkisyada istifade etmek olur.
+
+/*task.4 
+snake case-
+camelcase
+pascalcase
+tolowercase
+touppercase*/
