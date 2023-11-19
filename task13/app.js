ce8d78ce64b59a0cdefab70f95a7e0e18afd5712
Firuddin <firuddinaliyev888@gmail.com>
2023-11-19 12:59:21 +0400

    add

---
+++ task13/app.js
@@ -0,0 +1,10 @@
+let name=prompt("zəhmət olmasa adinizi daxil edin")
+if(name==="Ali"){
+    console.log("siz xos gelmisiz");
+}else if(name==="Sona"){
+console.log("siz Sona xanimsiz");
+}else if(name==="Gulu"){
+    console.log("siz gulu xanimsiz");
+}else{
+    console.log("zehmet olmasa ad daxil edin");
+}
