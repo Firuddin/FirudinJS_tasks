580d109eba6c388251dec9a5037144a89bf789c9
Firuddin <firuddinaliyev888@gmail.com>
2023-11-19 14:41:07 +0400

    addd

--- task13/app.js
+++ task13/app.js
@@ -1,4 +1,4 @@
-let name=prompt("zəhmət olmasa adinizi daxil edin")
+/*let name=prompt("zəhmət olmasa adinizi daxil edin")
 if(name==="Ali"){
     console.log("siz xos gelmisiz");
 }else if(name==="Sona"){
@@ -7,6 +7,15 @@ console.log("siz Sona xanimsiz");
     console.log("siz gulu xanimsiz");
 }else{
     console.log("zehmet olmasa ad daxil edin");
-}
+}*/
 
+
+let number=prompt("zəhmət olmasa ad daxil edin")
+if(number%15===0){
+    console.log("bu eded hem 3-e bolunur hemde 5-e");
+}else if(number%3===0){
+console.log("bu eded 3-e bolünür");
+}else if(number%5===0){
+    console.log("bu eded 5-e bolünür");
+}
 
